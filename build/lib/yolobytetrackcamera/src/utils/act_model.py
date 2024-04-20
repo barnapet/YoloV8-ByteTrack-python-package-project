00477cfdbd836ad29b31645851d35b5019856565
barnapet <barnapet00@gmail.com>
2024-04-20 09:14:44 +0200

    License file modified

--- build/lib/yolobytetrackcamera/src/utils/act_model.py
+++ build/lib/yolobytetrackcamera/src/utils/act_model.py
@@ -4,6 +4,6 @@ def model_choose(model_type):
     list_of_models = ['yolov8n.pt', 'yolov8s.pt', 'yolov8m.pt', 'yolov8l.pt', 'yolov8x.pt']
 
     if not 1 <= model_type <= len(list_of_models):
-        raise ValueError(f"Érvénytelen modellszám: {model_type}. Válasszon 1 és {len(list_of_models)} közötti számot!")
+        raise ValueError(f"Invalid model number: {model_type}. Choose a number between 1 and {len(list_of_models)}!")
 
     return YOLO(list_of_models[model_type-1])
